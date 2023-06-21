71a6968ffcfc931a68d0d8ad2baa770e4bcd058a
fer <fer@pop-os.localdomain>
2023-06-21 14:22:48 -0500

    Python

--- install_python.sh
+++ install_python.sh
@@ -2,6 +2,7 @@
 echo "Installing Python libraries..."
 echo "export WEBOTS_HOME=/usr/local/webots" >> ~/.bashrc
 echo "export PYTHONPATH=/usr/local/webots/lib/controller/python:$PYTHONPATH" >> ~/.bashrc
+source ~/.bashrc
 pip install numpy
 pip install scipy==1.7.2
 pip install pyyaml
